6f1b362aaaf3340f2aa82d4b0c87f3dbdf7ae870
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-27 22:57:17 -0800

    build: redirect good guess to location

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -9,7 +10,24 @@ image = "./blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pd.read_csv("./data/50_states.csv")
 
-user_answer = screen.textinput("Name the state", "What's another state name?")
+while True:
+
+    user_answer = screen.textinput("Name the state", "What's another state name?").title()
+
+
+    state_data = data[data.state == user_answer]
+
+    state_turtle = turtle.Turtle()
+    state_turtle.penup()
+    state_turtle.hideturtle()
+    x_coor = state_data["x"].values[0]
+    y_coor = state_data["y"].values[0]
+
+    state_turtle.goto(x=x_coor, y=y_coor)
+    state_turtle.write(arg=state_data["state"].values[0])
+
+
 
 screen.mainloop()
